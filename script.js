d5c3b569e5db90d3f203a9a785fbc3cf5ea4c5dd
varadkodgire <81140263+varadkodgire@users.noreply.github.com>
2023-10-28 10:44:27 +0530

    Update script.js
    trying to store the timers on the users browser

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const timersContainer = document.getElementById('timers');
 const addTimerButton = document.getElementById('addTimerButton');
 
-// Call createNewTimer when the page loads
-createNewTimer();
+// Call a function to load timers from localStorage on page load
+loadTimers();
 
 addTimerButton.addEventListener('click', createNewTimer);
 
@@ -42,7 +42,9 @@ function createNewTimer() {
   }
 
   function deleteTimer() {
-    timersContainer.removeChild(timerElement); // Remove the timer element
+    timersContainer.removeChild(timerElement);
+    // Call a function to save the updated timers to localStorage
+    saveTimers();
   }
 
   function updateTimeDisplay() {
@@ -52,24 +54,75 @@ function createNewTimer() {
       timerDisplay.textContent = `${formatTime(elapsedTime)}`;
     }
   }
-  
+
   function formatTime(seconds) {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     seconds = Math.floor(seconds % 60);
-    return `${hours}h:${minutes}m:${seconds}s`;
+    return `${hours}h${minutes}m${seconds}s`;
   }
-  
+
   toggleButton.addEventListener('click', toggleTimer);
-  deleteButton.addEventListener('click', deleteTimer); // Add click event for delete button
+  deleteButton.addEventListener('click', deleteTimer);
   
   timerElement.appendChild(activityName);
   timerElement.appendChild(toggleButton);
-  timerElement.appendChild(deleteButton); // Add the delete button
+  timerElement.appendChild(deleteButton);
   timerElement.appendChild(timerDisplay);
   
   timersContainer.appendChild(timerElement);
-  
-  // Initially display the timer with elapsed time
-  timerDisplay.textContent = `${formatTime(elapsedTime)}`;
+
+  // Call a function to save the updated timers to localStorage
+  saveTimers();
+}
+
+function saveTimers() {
+  const timers = [];
+  const timerElements = timersContainer.querySelectorAll('.timer');
+
+  timerElements.forEach((timerElement) => {
+    // Extract timer data from each timer element
+    const activityName = timerElement.querySelector('input').value;
+    const timerDisplay = timerElement.querySelector('.timerDisplay').textContent;
+    timers.push({ activityName, timerDisplay });
+  });
+
+  // Save the timers to localStorage as a JSON string
+  localStorage.setItem('timers', JSON.stringify(timers));
+}
+
+function loadTimers() {
+  // Retrieve timers from localStorage
+  const storedTimers = localStorage.getItem('timers');
+
+  if (storedTimers) {
+    const timers = JSON.parse(storedTimers);
+
+    // Create timer elements based on the stored data
+    timers.forEach((timerData) => {
+      const timerElement = document.createElement('div');
+      timerElement.className = 'timer';
+
+      const activityName = document.createElement('input');
+      activityName.type = 'text';
+      activityName.value = timerData.activityName; // Set the saved activity name
+
+      const toggleButton = document.createElement('button');
+      toggleButton.textContent = 'Start';
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+
+      const timerDisplay = document.createElement('div');
+      timerDisplay.className = 'timerDisplay';
+      timerDisplay.textContent = timerData.timerDisplay; // Set the saved timer display
+
+      timerElement.appendChild(activityName);
+      timerElement.appendChild(toggleButton);
+      timerElement.appendChild(deleteButton);
+      timerElement.appendChild(timerDisplay);
+
+      timersContainer.appendChild(timerElement);
+    });
+  }
 }
